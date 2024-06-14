feb26ad502718f32a29cfd00ff98c809c886a685
Jeffrey Cornillie <jeffrey.cornillie@student.howest.be>
2024-06-14 23:15:40 +0200

    Add first draft of project

---
+++ KeySplash/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KeySplash.SplashScreens;
+
+namespace KeySplash;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private const string ResourceLocation = "/resources/bongo_idle.jpg";
+    private bool _isStarted;
+    private bool _isTimerReset;
+    private CustomSplashScreen _currentCustomSplashScreen;
+    private bool _isKeyDown;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    public void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (_isKeyDown || !_isStarted) return;
+        _isKeyDown = true;
+        if (Application.Current.Windows.OfType<CustomSplashScreen>().FirstOrDefault() is CustomSplashScreen
+            OpenSplashScreen)
+        {
+            OpenSplashScreen.ResetClosing();
+            if(OpenSplashScreen is BongoCat bongoCat) bongoCat.Tap();
+            return;
+        }
+        _currentCustomSplashScreen = ShowBongo();
+        HideSplash(_currentCustomSplashScreen);
+    }
+
+    private CustomSplashScreen ShowSplash(string resource)
+    {
+        CustomSplashScreen splash = new CustomSplashScreen(this,resource);
+        splash.Show();
+        return splash;
+    }
+    private CustomSplashScreen ShowBongo()
+    {
+        CustomSplashScreen splash = new BongoCat(this);
+        splash.Show();
+        return splash;
+    }
+    private void HideSplash(CustomSplashScreen? splash, bool isInstant = false)
+    {
+        if (splash is null) return;
+        if (isInstant)
+        {
+            splash.Close();
+            return;
+        }
+        var timeSpanForFade = new TimeSpan(0, 0, 0, 0,500 );
+        splash.CloseDelayed(timeSpanForFade);
+    }
+
+    public void MainWindow_OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (!_isStarted) return;
+        foreach (Key key in Enum.GetValues(typeof(Key)))
+        {
+            if (key == Key.None) continue;
+            if (Keyboard.IsKeyDown(key)) return;
+        }
+        _isKeyDown = false;
+    }
+
+    private void BtnStart_OnClick(object sender, RoutedEventArgs e)
+    {
+        _isStarted = !_isStarted;
+        btnStart.Content = _isStarted ? "Stop" : "Start";
+    }
+}
---
+++ KeySplash/SplashScreens/Base/CustomSplashScreen.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace KeySplash;
+
+public partial class CustomSplashScreen : Window
+{
+    protected string Resource { get; set; }
+    private TimeSpan _fadeDuration = new (0, 0, 0,1);
+    private TimeSpan _closeDuration;
+    private DateTime _endFadeTime;
+    private DateTime _endCloseTime;
+    private bool _stopClosing;
+
+    public CustomSplashScreen(Window window,string resource)
+    {
+        InitializeComponent();
+        Owner = window;
+        Uri imageUri = new Uri(resource, UriKind.Relative);
+        Image.Source = new BitmapImage(imageUri);
+        Resource = resource;
+    }
+
+    private void CustomSplashScreen_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        this.Width = Image.Width;
+        this.Height = Image.Height;
+    }
+    private async void FadeClose(TimeSpan timeSpan)
+    {
+        _endFadeTime = DateTime.Now + timeSpan;
+        DateTime now = DateTime.Now;
+        while (DateTime.Compare(now,_endFadeTime) < 0)
+        {
+            Opacity = (_endFadeTime - now).Ticks / (double)timeSpan.Ticks;
+            await Task.Delay(1);
+            now = DateTime.Now;
+            if (_stopClosing)
+            {
+                _stopClosing = false;
+                Opacity = 1;
+                CloseDelayed(_closeDuration);
+                return;
+            }
+        }
+        base.Close(); 
+    }
+    public async void CloseDelayed(TimeSpan timeSpan)
+    {
+        _closeDuration = timeSpan;
+        _endCloseTime = DateTime.Now + timeSpan;
+        while (DateTime.Compare(DateTime.Now,_endCloseTime) < 0)
+        {
+            await Task.Delay(1);
+            if (_stopClosing)
+            {
+                _stopClosing = false;
+                _endCloseTime = DateTime.Now + timeSpan;
+            }
+        }
+        FadeClose(_fadeDuration);
+    }
+
+    public void ResetClosing()
+    {
+        _stopClosing = true;
+    }
+
+    private void CustomSplashScreen_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        ((MainWindow)Owner).MainWindow_OnKeyDown(sender,e);
+    }
+
+    private void CustomSplashScreen_OnKeyUp(object sender, KeyEventArgs e)
+    {
+        ((MainWindow)Owner).MainWindow_OnKeyUp(sender,e);
+    }
+}
